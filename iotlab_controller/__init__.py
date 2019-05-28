0c35b8cdb5c248c25a58b7045302bb1fe8b4c027
Martine Lenders <m.lenders@fu-berlin.de>
2019-05-28 14:16:51 +0200

    iotlab_controller: import get_default_api

--- iotlab_controller/__init__.py
+++ iotlab_controller/__init__.py
@@ -6,6 +6,7 @@
 #
 # Distributed under terms of the MIT license.
 
+from common import get_default_api
 from experiment import BaseExperiment
 from firmware import BaseFirmware
 from nodes import BaseNodes
