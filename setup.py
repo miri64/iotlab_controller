099bb09da9f1917023e551347301c0bd6c9e70ce
Martine S. Lenders <m.lenders@fu-berlin.de>
2019-09-03 15:32:48 +0200

    setup.py: bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import os
 import sys
 
 name = "iotlab_controller"
-version = "0.2.3a"
+version = "0.2.4a"
 description = "Python-based controller for IoT-LAB experiments"
 author = "Martine Lenders"
 author_email = "m.lenders@fu-berlin.de"
