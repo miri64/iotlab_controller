11bb62c4de8db941f01d44c4d6b5f3c2cef9e911
Hauke Petersen <hauke.petersen@fu-berlin.de>
2020-08-19 12:32:14 +0200

    setup.py: bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import os
 import sys
 
 name = "iotlab_controller"
-version = "0.4.2a"
+version = "0.4.3a"
 description = "Python-based controller for IoT-LAB experiments"
 author = "Martine Lenders"
 author_email = "m.lenders@fu-berlin.de"
