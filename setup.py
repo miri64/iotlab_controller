d74a1fb525e8cdf3a6afe0f26a78a643693f1ca8
Martine Lenders <m.lenders@fu-berlin.de>
2021-03-19 11:00:46 +0100

    Bump to 0.7.0b

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages
 
 name = "iotlab_controller"
-version = "0.6.1b"
+version = "0.7.0b"
 description = "Python-based controller for IoT-LAB experiments"
 author = "Martine Lenders"
 author_email = "m.lenders@fu-berlin.de"
